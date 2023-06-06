1851b98d370fa43ff116ecb3a9f519afecd77603
PREETI2096 <preetirg96@gmail.com>
2023-06-06 14:44:18 +0530

    Added: sorting problems

---
+++ DSAlgorithms/DSAlgorithms/BubbleSort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAlgorithms
+{
+    internal class BubbleSort
+    {
+        public void GetBubbleSort()
+        {
+            int[] a = { 9, 20, 55, 79, 44, 68, 66, 54, 35, 37, 25 };
+
+            // for loop for printing the elements of an array
+            Console.WriteLine("Elements of array are :");
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(" " + a[i] + " ");
+            }
+            // nested for loop for bubble sort operation
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < a.Length - i - 1; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        int temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("\nElements of array after bubble sort is :");
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(" " + a[i] + " ");
+            }
+        }
+    }
+}
---
+++ DSAlgorithms/DSAlgorithms/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DSAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Problems");
+              BubbleSort bubbleSort = new BubbleSort();
+              bubbleSort.GetBubbleSort();
+           
+            int[] numbers = new int[10] { 2, 5, -4, 11, 0, 18, 22, 67, 51, 6 };
+            Console.WriteLine("\nOriginal Array Elements :");
+            InsertionSort.PrintIntegerArray(numbers);
+            Console.WriteLine("\nSorted Array Elements after insertion sort :");
+           InsertionSort.PrintIntegerArray(InsertionSort.GetInsertionSort(numbers));
+            Console.WriteLine("\n");
+            MergeSort mergeSort = new MergeSort();
+            mergeSort.Sort();
+        }
+
+    }
+    }
